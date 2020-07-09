4f4a898fa544b1a809f9fab2a8bbc9d5ca1551a6
Daniel Vasquez <andresvp2002@gmail.com>
2020-07-08 21:37:05 -0500

    About Us completed
    i completed the about us page and manage to solve the problem of placing an image as background semifaded so i can place other elements on it.

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {
-    Media,Button
+    Media,Button, Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle,  CardGroup, CardFooter,  CardDeck
   } from 'reactstrap';
 
+  import optimax from '../edits-32.JPG'
 function About(props) {
 return(
 <body>
     <div className="container">
-        <div className="row row-content">
+    
+                   
+                 
+              
+        
             <div className="row">
-                <h1>Nuestra historia</h1>
-            </div>
-            <div className="row">
-                <div className="col-12 col-sm-6 align-self-center">
+                <div className="offset-2 col-8 offset-md-0 col-sm-6 align-self-center">
                     <h1>OPTIMAX</h1>
                     <p>Inició en septiembre del 2005 con la idea de tener un negocio 
                        familiar como apoyo económico.
@@ -21,7 +24,7 @@ return(
                     Luego con base a las recomendaciones de los clientes empezamos
                     a buscar capacitaciones y asesorías de proveedores y laboratorios
                     especialistas en productos, calidad de lentes, monturas y lo
-                    mejor en trámite de pedidos y venta de fórmulas.Nuestro lema es:</p>
+                    mejor en trámite de pedidos y venta de fórmulas.</p><p>Nuestro lema es:</p>
                 </div>
                 <div className="col-12 col-sm-6">
                     <h1>" Atención personalizada
@@ -37,8 +40,48 @@ y para cada presupuesto. "</h1>
         
         </div>
    
-    </div>
+    
         
+  
+    <div className=" banner">
+    <img src={optimax} alt="optimax-1" />
+    <div className="content align-self-center">
+        <div className="row  ">
+            <div className="  textwhite ">
+            <CardDeck className="col-12 ">
+            <Card className="d-none d-sm-block" >
+                <CardBody className="text-center" >
+                <span className="fa fa-check"></span>
+                <CardTitle >Experiencia</CardTitle>
+                <CardText className="d-none d-md-block"  >Tenemos más de 15 años de experiencia
+                     para ofrecerte servicios de la mejor calidad
+                      y cumplir tus expectativas.</CardText>
+                </CardBody>
+            </Card>
+            <Card >
+                <CardBody  className="text-center">
+                <span className="fa fa-check"></span>
+
+                <CardTitle className="col-12 ">Trato al cliente</CardTitle>
+                <CardText className="d-none d-md-block">Uno de los pilares fundamentales de nuestra empresa
+                     es el trato adecuado a nuestros clientes y así establecer
+                      confianza y respeto.</CardText>
+                </CardBody>
+            </Card>
+            <Card   >
+                <CardBody className="text-center">
+                <span className="fa fa-check"></span>
+                <CardTitle className="col-12 ">Excelente calidad</CardTitle>
+                <CardText className="d-none d-md-block">Creemos que vale la pena invertir
+                     en tus ojos, por eso manejamos las mejores
+                      marcas de lentes y monturas.</CardText>
+                </CardBody>
+            </Card>
+            </CardDeck>
+            </div>
+        </div>
+    </div>
+    </div>
     <div className="row row-content bgGreen">
         <div className="row">
 
@@ -52,6 +95,7 @@ y para cada presupuesto. "</h1>
             </div>
         </div>
     </div> 
+    
   
 </body>
 );
